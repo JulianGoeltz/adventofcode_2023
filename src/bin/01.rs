229a415c87572805b9aa1aeecc9ea236472c8156
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-12-01 20:55:17 +0100

    01: restructuring for part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,28 +5,32 @@ fn main() {
      * Dummy
      *************************/
     let file_path = "inputs/01_dummy.txt";
-    let tmp = countup(file_path);
+    let tmp = 
+        sum_vector(
+            select_digits_to_create_numbers(
+                get_digits(file_path)));
     println!("Dummy: The sum of the numbers is {tmp}");
 
     /*************************
      * First Input
      *************************/
     let file_path = "inputs/01_first.txt";
-    let tmp = countup(file_path);
+    let tmp = 
+        sum_vector(
+            select_digits_to_create_numbers(
+                get_digits(file_path)));
     println!("First Input: The sum of the numbers is {tmp}");
 }
 
-fn countup(file_path: &str) -> u64 {
-    println!("Read file {}", file_path);
-
+fn get_digits(file_path: &str) -> Vec<Vec<u64>> {
+    /*************************
+     * Go through lines and create linewise list of numbers
+     *************************/
     // Read file contents
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    /*************************
-     * Go through lines and create linewise list of numbers
-     *************************/
-    let mut vector_of_numerical_lines = Vec::new();
+    let mut vector_of_numerical_lines:Vec<Vec<u64>> = Vec::new();
     for line in contents.lines() {
         let mut digits: Vec<u64> = Vec::new();
         for char in line.chars() {
@@ -37,30 +41,22 @@ fn countup(file_path: &str) -> u64 {
         }
         vector_of_numerical_lines.push(digits);
     }
+    return vector_of_numerical_lines;
+}
 
+fn select_digits_to_create_numbers(vector_of_numerical_lines: Vec<Vec<u64>>) -> Vec<u64> {
     /*************************
      * Select first and last per line
      *************************/
     let mut vector_of_numbers:Vec<u64> = Vec::new();
-    let mut i = 0;
     for line_vec in &vector_of_numerical_lines {
-        if line_vec.len() == 0 {
-            println!("Error in line {i}");
-            // lol, all this hassle because I had an addditional \n in the input, i.e. empty last
-            // line
-            let mut j = 0;
-            for line in contents.lines() {
-                if j == i {
-                    println!("Line: {}", line);
-                }
-                j += 1;
-            }
-        }
         let tmp = 10 * line_vec[0] + line_vec[line_vec.len() - 1];
         vector_of_numbers.push(tmp);
-        i += 1;
     }
+    return vector_of_numbers;
+}
 
+fn sum_vector(vector_of_numbers: Vec<u64>) -> u64 {
     /*************************
      * Sum up
      *************************/
