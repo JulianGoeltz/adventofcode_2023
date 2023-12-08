131296bc65d1a520a0c8a6311e122fd4eb65b5a2
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-12-08 22:49:04 +0100

    cargo fmt

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,38 +5,30 @@ fn main() {
      * Dummy
      *************************/
     let file_path = "inputs/01_dummy.txt";
-    let tmp =
-        sum_vector(
-            select_digits_to_create_numbers(
-                get_digits(file_path)));
+    let tmp = sum_vector(select_digits_to_create_numbers(get_digits(file_path)));
     println!("Dummy: The sum of the numbers is {tmp}");
 
     /*************************
      * First Input
      *************************/
     let file_path = "inputs/01_first.txt";
-    let tmp =
-        sum_vector(
-            select_digits_to_create_numbers(
-                get_digits(file_path)));
+    let tmp = sum_vector(select_digits_to_create_numbers(get_digits(file_path)));
     println!("First Input: The sum of the numbers is {tmp}");
     /*************************
      * Second dummy
      *************************/
     let file_path = "inputs/01_dummy2.txt";
-    let tmp =
-        sum_vector(
-            select_digits_to_create_numbers(
-                get_digits_also_alphabetical(file_path)));
+    let tmp = sum_vector(select_digits_to_create_numbers(
+        get_digits_also_alphabetical(file_path),
+    ));
     println!("second dummy: The sum of the numbers is {tmp}");
     /*************************
      * Second part with old input
      *************************/
     let file_path = "inputs/01_first.txt";
-    let tmp =
-        sum_vector(
-            select_digits_to_create_numbers(
-                get_digits_also_alphabetical(file_path)));
+    let tmp = sum_vector(select_digits_to_create_numbers(
+        get_digits_also_alphabetical(file_path),
+    ));
     println!("Input with other rules: The sum of the numbers is {tmp}");
 }
 
@@ -45,26 +37,20 @@ fn get_digits_also_alphabetical(file_path: &str) -> Vec<Vec<u64>> {
      * Go through lines and create linewise list of numbers
      *************************/
     // Read file contents
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let vec_replace:Vec<String>= [
+    let vec_replace: Vec<String> = [
         // "0zero",
-        "1one",
-        "2two",
-        "3three",
-        "4four",
-        "5five",
-        "6six",
-        "7seven",
-        "8eight",
-        "9nine",
-    ].iter().map(|&s| s.into()).collect();
+        "1one", "2two", "3three", "4four", "5five", "6six", "7seven", "8eight", "9nine",
+    ]
+    .iter()
+    .map(|&s| s.into())
+    .collect();
 
-    let mut vector_of_numerical_lines:Vec<Vec<u64>> = Vec::new();
+    let mut vector_of_numerical_lines: Vec<Vec<u64>> = Vec::new();
     for line in contents.lines() {
         let mut digits: Vec<u64> = Vec::new();
-        let mut lines:Vec<String> = Vec::new();
+        let mut lines: Vec<String> = Vec::new();
         lines.push(line.to_string());
         let mut keepgoing = true;
         while keepgoing {
@@ -83,7 +69,7 @@ fn get_digits_also_alphabetical(file_path: &str) -> Vec<Vec<u64>> {
                             index_number = i;
                             keepgoing = true;
                         }
-                    },
+                    }
                     None => (),
                 }
                 i += 1;
@@ -93,7 +79,7 @@ fn get_digits_also_alphabetical(file_path: &str) -> Vec<Vec<u64>> {
                 let tmp = vec_replace[index_number].clone();
                 let _replace = &tmp[0..1].to_string();
                 let mut line_new = lines[lines.len() - 1].clone();
-                line_new.replace_range(index_find..index_find+1, _replace);
+                line_new.replace_range(index_find..index_find + 1, _replace);
                 lines.push(line_new);
             }
         }
@@ -103,7 +89,10 @@ fn get_digits_also_alphabetical(file_path: &str) -> Vec<Vec<u64>> {
         for char in line_new.chars() {
             if char.is_numeric() {
                 digits.push(
-                    char.to_string().parse::<u64>().expect("no conversion possible"));
+                    char.to_string()
+                        .parse::<u64>()
+                        .expect("no conversion possible"),
+                );
             }
         }
         vector_of_numerical_lines.push(digits);
@@ -116,16 +105,18 @@ fn get_digits(file_path: &str) -> Vec<Vec<u64>> {
      * Go through lines and create linewise list of numbers
      *************************/
     // Read file contents
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let mut vector_of_numerical_lines:Vec<Vec<u64>> = Vec::new();
+    let mut vector_of_numerical_lines: Vec<Vec<u64>> = Vec::new();
     for line in contents.lines() {
         let mut digits: Vec<u64> = Vec::new();
         for char in line.chars() {
             if char.is_numeric() {
                 digits.push(
-                    char.to_string().parse::<u64>().expect("no conversion possible"));
+                    char.to_string()
+                        .parse::<u64>()
+                        .expect("no conversion possible"),
+                );
             }
         }
         vector_of_numerical_lines.push(digits);
@@ -137,7 +128,7 @@ fn select_digits_to_create_numbers(vector_of_numerical_lines: Vec<Vec<u64>>) -> 
     /*************************
      * Select first and last per line
      *************************/
-    let mut vector_of_numbers:Vec<u64> = Vec::new();
+    let mut vector_of_numbers: Vec<u64> = Vec::new();
     for line_vec in &vector_of_numerical_lines {
         let tmp = 10 * line_vec[0] + line_vec[line_vec.len() - 1];
         vector_of_numbers.push(tmp);
@@ -149,7 +140,7 @@ fn sum_vector(vector_of_numbers: Vec<u64>) -> u64 {
     /*************************
      * Sum up
      *************************/
-    let mut sum:u64 = 0;
+    let mut sum: u64 = 0;
     for number in &vector_of_numbers {
         sum += number;
     }
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,14 +1,18 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
 fn main() {
     let file_path = "inputs/02_dummy.txt";
     let vector_games = create_game_vec(file_path);
 
-    println!(""); println!("");
-    let tmp = check_condition(vector_games.clone(),
-                              HashMap::from([(0, 12), (1, 13), (2, 14)]));
-    println!(""); println!("");
+    println!("");
+    println!("");
+    let tmp = check_condition(
+        vector_games.clone(),
+        HashMap::from([(0, 12), (1, 13), (2, 14)]),
+    );
+    println!("");
+    println!("");
     println!("The indices of the valid games sum to {tmp}");
 
     let tmp = check_condition2(vector_games);
@@ -18,10 +22,14 @@ fn main() {
     let file_path = "inputs/02_first.txt";
     let vector_games = create_game_vec(file_path);
 
-    println!(""); println!("");
-    let tmp = check_condition(vector_games.clone(),
-                              HashMap::from([(0, 12), (1, 13), (2, 14)]));
-    println!(""); println!("");
+    println!("");
+    println!("");
+    let tmp = check_condition(
+        vector_games.clone(),
+        HashMap::from([(0, 12), (1, 13), (2, 14)]),
+    );
+    println!("");
+    println!("");
     println!("The indices of the valid games sum to {tmp}");
     let tmp = check_condition2(vector_games);
     println!("The sum of the power of the minimums is {tmp}");
@@ -29,7 +37,7 @@ fn main() {
 }
 
 fn check_condition2(vector_games: HashMap<i32, Vec<Vec<u64>>>) -> u64 {
-    let mut sum:u64 = 0;
+    let mut sum: u64 = 0;
     for (gameindex, subsets) in vector_games.iter() {
         let mut minimum: Vec<u64> = vec![0, 0, 0];
         for subset in subsets {
@@ -47,9 +55,11 @@ fn check_condition2(vector_games: HashMap<i32, Vec<Vec<u64>>>) -> u64 {
     return sum;
 }
 
-fn check_condition(vector_games: HashMap<i32, Vec<Vec<u64>>>,
-                   comparison: HashMap<usize, u64>) -> i32 {
-    let mut sum:i32 = 0;
+fn check_condition(
+    vector_games: HashMap<i32, Vec<Vec<u64>>>,
+    comparison: HashMap<usize, u64>,
+) -> i32 {
+    let mut sum: i32 = 0;
     for (gameindex, subsets) in &vector_games {
         let mut correct = true;
         for subset in subsets {
@@ -68,31 +78,28 @@ fn check_condition(vector_games: HashMap<i32, Vec<Vec<u64>>>,
 }
 
 fn create_game_vec(file_path: &str) -> HashMap<i32, Vec<Vec<u64>>> {
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     // let mut vector_games:Vec<Vec<Vec<u64>>> = Vec::new();
     let mut vector_games = HashMap::new();
     for line in contents.lines() {
         // println!("{}", line);
         let mut line_rest = line;
-        let mut gameindex:i32=-1;
+        let mut gameindex: i32 = -1;
         // find game index
         match line_rest.find(":") {
             Some(index) => {
-                gameindex = line_rest[5..index].
-                    parse::<i32>().expect("No conversion possible");
-                line_rest = &line_rest[index+2..];
+                gameindex = line_rest[5..index]
+                    .parse::<i32>()
+                    .expect("No conversion possible");
+                line_rest = &line_rest[index + 2..];
                 // println!("found at {index} {gameindex};; {line_rest}");
-            },
+            }
             None => (),
         }
         // find subsets
 
-        vector_games.insert(
-            gameindex,
-            identify_subsets(line_rest)
-        );
+        vector_games.insert(gameindex, identify_subsets(line_rest));
     }
 
     return vector_games;
@@ -100,25 +107,21 @@ fn create_game_vec(file_path: &str) -> HashMap<i32, Vec<Vec<u64>>> {
 
 fn parse_subset(line: &str) -> Vec<u64> {
     // println!("trying to parse '{line}'");
-    let mut subset:Vec<u64> = vec![0, 0, 0];
+    let mut subset: Vec<u64> = vec![0, 0, 0];
 
-    let colours:HashMap<&str, usize> = HashMap::from([
-        ("red", 0),
-        ("green", 1),
-        ("blue", 2),
-    ]);
+    let colours: HashMap<&str, usize> = HashMap::from([("red", 0), ("green", 1), ("blue", 2)]);
 
     for (c_name, c_id) in &colours {
         for substring in line.split(", ") {
             match substring.find(c_name) {
                 Some(index) => {
-                    let tmp: Vec<&str> = line[0..index-1].matches(char::is_numeric).collect();
-                    let tmp = &substring[0..index-1];
+                    let tmp: Vec<&str> = line[0..index - 1].matches(char::is_numeric).collect();
+                    let tmp = &substring[0..index - 1];
                     // println!("{tmp}");
                     subset[*c_id] = tmp.parse::<u64>().expect("No conversion possible");
                     let tmp = subset[*c_id];
                     // println!("colour {c_name} found at {index} with value {tmp}");
-                },
+                }
                 None => (),
             }
         }
@@ -127,30 +130,22 @@ fn parse_subset(line: &str) -> Vec<u64> {
 }
 
 fn identify_subsets(line: &str) -> Vec<Vec<u64>> {
-    let mut vector_subsets:Vec<Vec<u64>> = Vec::new();
+    let mut vector_subsets: Vec<Vec<u64>> = Vec::new();
     let mut keepgoing = true;
     let mut line_rest = line.to_string();
     while keepgoing {
         keepgoing = false;
         match line_rest.find(";") {
             Some(index) => {
-                vector_subsets.push(
-                    parse_subset(
-                        &line_rest[0..index]
-                        )
-                    );
-                line_rest = line_rest[index+2..].to_string();
+                vector_subsets.push(parse_subset(&line_rest[0..index]));
+                line_rest = line_rest[index + 2..].to_string();
                 // println!("found at {index} {gameindex};; {line_rest}");
                 keepgoing = true;
-            },
+            }
             None => (),
         }
     }
     // also parse the last subset
-    vector_subsets.push(
-        parse_subset(
-            &line_rest
-            )
-        );
+    vector_subsets.push(parse_subset(&line_rest));
     return vector_subsets;
 }
