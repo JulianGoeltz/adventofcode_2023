d0698c2f0f293748ab2d6ee26dab693d8b0d3d14
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-12-09 17:52:34 +0100

    03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -4,26 +4,108 @@ use std::fs;
 fn main() {
     let file_path = "inputs/03_dummy.txt";
     println!("On file {file_path}");
-    do_the_thing(file_path);
+    let tmp = do_the_thing(file_path);
+    println!("Total number is {tmp}");
+    assert_eq!(tmp, 4361);
 
     let file_path = "inputs/03_input.txt";
     println!("On file {file_path}");
-    do_the_thing(file_path);
-}
+    let tmp = do_the_thing(file_path);
+    println!("Total number is {tmp}");
+    assert_eq!(tmp, 559667);
 
-fn do_the_thing(file_path: &str) {
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("======================== new rule: ratios");
+    let file_path = "inputs/03_dummy.txt";
+    println!("On file {file_path}");
+    let tmp = do_the_other_thing(file_path);
+    println!("Total number is {tmp}");
+    assert_eq!(tmp, 467835);
+
+    let file_path = "inputs/03_input.txt";
+    println!("On file {file_path}");
+    let tmp = do_the_other_thing(file_path);
+    println!("Total number is {tmp}");
+    assert_eq!(tmp, 86841457);
 
     println!();
+}
 
-    // println!("number of chars is {}", contents.len());
-    // println!("number of lines is {}", contents.lines().len());
+fn get_shifted_boundaries(m_start: usize, m_end: usize, line_len: usize) -> (usize, usize) {
+    let start: usize = match m_start {
+        0 => 0,
+        _ => m_start - 1,
+    };
+    let mut end: usize = m_end;
+    if end >= line_len {
+        end -= 1
+    };
+    (start, end)
+}
 
-    //
-    // let mut vector_of_lines : Vec<Vec<char>> = vec![
-    //     0; len];
+fn do_the_other_thing(file_path: &str) -> u64 {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let vector_of_lines: Vec<String> = contents.lines().map(String::from).collect();
+    let regex_number: Regex = Regex::new(r"[0-9]+").unwrap();
 
+    let mut vector_of_ratios: Vec<u64> = Vec::new();
+
+    for (i_line, line) in vector_of_lines.iter().enumerate() {
+        for (m_idx, _m_str) in line.match_indices('*') {
+            // println!("{}", line);
+            // println!("{} {}", m_idx, m_str);
+            let mut vector_of_numbers: Vec<u64> = Vec::new();
+            // search above
+            if i_line > 0 {
+                // println!("prvs line {}", vector_of_lines[i_line - 1]);
+                for m in regex_number.find_iter(&vector_of_lines[i_line - 1]) {
+                    let (start, end) = get_shifted_boundaries(m.start(), m.end(), line.len());
+                    // println!("index gear {}, start&end {} {}", m_idx, start, end);
+                    if start <= m_idx && end >= m_idx {
+                        vector_of_numbers.push(m.as_str().parse::<u64>().expect("No conversion possible"));
+                        // println!("found number {}", &vector_of_numbers.last().unwrap());
+                    }
+                }
+            }
+            // search below
+            if i_line < vector_of_lines.len() - 1 {
+                // println!("next line {}", vector_of_lines[i_line + 1]);
+                for m in regex_number.find_iter(&vector_of_lines[i_line + 1]) {
+                    let (start, end) = get_shifted_boundaries(m.start(), m.end(), line.len());
+                    // println!("index gear {}, start&end {} {}", m_idx, start, end);
+                    if start <= m_idx && end >= m_idx {
+                        vector_of_numbers.push(m.as_str().parse::<u64>().expect("No conversion possible"));
+                        // println!("found number {}", &vector_of_numbers.last().unwrap());
+                    }
+                }
+            }
+            // search in same line
+            {
+                for m in regex_number.find_iter(line) {
+                    let (start, end) = get_shifted_boundaries(m.start(), m.end(), line.len());
+                    // println!("index gear {}, start&end {} {}", m_idx, start, end);
+                    if start == m_idx || end == m_idx {
+                        vector_of_numbers.push(m.as_str().parse::<u64>().expect("No conversion possible"));
+                        // println!("found number {}", &vector_of_numbers.last().unwrap());
+                    }
+                }
+            }
+
+            // see if it is exactly two numbers
+            if vector_of_numbers.len() == 2 {
+                vector_of_ratios.push(vector_of_numbers.iter().product::<u64>());
+                // println!("exactly two numbers! added {}", &vector_of_ratios.last().unwrap());
+            }
+
+        }
+    }
+
+    vector_of_ratios.iter().sum::<u64>()
+}
+
+
+fn do_the_thing(file_path: &str) -> u64 {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let vector_of_lines: Vec<String> = contents.lines().map(String::from).collect();
     let regex_number: Regex = Regex::new(r"[0-9]+").unwrap();
 
     let mut vector_of_numbers: Vec<u64> = Vec::new();
@@ -34,14 +116,7 @@ fn do_the_thing(file_path: &str) {
             // println!("  {} {} {} {}", m.as_str(), m.start(), m.end(), m.len());
             let mut has_symbol_adjacent = false;
             // define extended sizes for diagonal check
-            let start: usize = match m.start() {
-                0 => 0,
-                _ => m.start() - 1,
-            };
-            let mut end: usize = m.end();
-            if end >= line.len() {
-                end -= 1
-            };
+            let (start, end) = get_shifted_boundaries(m.start(), m.end(), line.len());
 
             // check the adjacent chars
             if is_special_char(line.chars().nth(start).expect("index of start is wrong"))
@@ -82,7 +157,7 @@ fn do_the_thing(file_path: &str) {
         }
     }
 
-    println!("Total number is {}", vector_of_numbers.iter().sum::<u64>());
+    vector_of_numbers.iter().sum::<u64>()
 }
 
 fn is_special_char(c: char) -> bool {
