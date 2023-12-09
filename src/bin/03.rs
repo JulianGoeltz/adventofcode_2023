b8e991af1dd75dcdeb36ee1d4a91dc017200e654
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-12-09 17:15:42 +0100

    03: 01 fmt

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
 fn main() {
     let file_path = "inputs/03_dummy.txt";
@@ -9,7 +9,6 @@ fn main() {
     let file_path = "inputs/03_input.txt";
     println!("On file {file_path}");
     do_the_thing(file_path);
-
 }
 
 fn do_the_thing(file_path: &str) {
@@ -23,14 +22,11 @@ fn do_the_thing(file_path: &str) {
     //
     // let mut vector_of_lines : Vec<Vec<char>> = vec![
     //     0; len];
-    let vector_of_lines : Vec<String> = contents
-        .lines()
-        .map(String::from)
-        .collect();
+    let vector_of_lines: Vec<String> = contents.lines().map(String::from).collect();
 
-    let regex_number : Regex = Regex::new(r"[0-9]+").unwrap();
+    let regex_number: Regex = Regex::new(r"[0-9]+").unwrap();
 
-    let mut vector_of_numbers : Vec<u64> = Vec::new();
+    let mut vector_of_numbers: Vec<u64> = Vec::new();
 
     for (i_line, line) in vector_of_lines.iter().enumerate() {
         // println!("{line}");
@@ -38,53 +34,39 @@ fn do_the_thing(file_path: &str) {
             // println!("  {} {} {} {}", m.as_str(), m.start(), m.end(), m.len());
             let mut has_symbol_adjacent = false;
             // define extended sizes for diagonal check
-            let start : usize = match m.start() {
+            let start: usize = match m.start() {
                 0 => 0,
                 _ => m.start() - 1,
             };
-            let mut end : usize = m.end();
-            if end >= line.len() {end -= 1};
+            let mut end: usize = m.end();
+            if end >= line.len() {
+                end -= 1
+            };
 
             // check the adjacent chars
-            if
-                is_special_char(line
-                                .chars()
-                                .nth(start)
-                                .expect("index of start is wrong")
-                                )
-                ||
-                is_special_char(line
-                                .chars()
-                                .nth(end)
-                                .expect("index of end is wrong")
-                                )
+            if is_special_char(line.chars().nth(start).expect("index of start is wrong"))
+                || is_special_char(line.chars().nth(end).expect("index of end is wrong"))
             {
-                    has_symbol_adjacent = true;
+                has_symbol_adjacent = true;
             }
             // check previous line if not the first line
             if i_line > 0 {
-                 // println!("previous line {}", vector_of_lines[i_line - 1]);
-                 if vector_of_lines[i_line - 1][start..end + 1]
-                     .contains(is_special_char)
-                 {
-                     has_symbol_adjacent = true;
-                 }
+                // println!("previous line {}", vector_of_lines[i_line - 1]);
+                if vector_of_lines[i_line - 1][start..end + 1].contains(is_special_char) {
+                    has_symbol_adjacent = true;
+                }
             }
             // check next line if not the last line
             if i_line < vector_of_lines.len() - 1 {
-                 // println!("previous line {}", vector_of_lines[i_line + 1]);
-                 if vector_of_lines[i_line + 1][start..end + 1]
-                     .contains(is_special_char)
-                 {
-                     has_symbol_adjacent = true;
-                 }
+                // println!("previous line {}", vector_of_lines[i_line + 1]);
+                if vector_of_lines[i_line + 1][start..end + 1].contains(is_special_char) {
+                    has_symbol_adjacent = true;
+                }
             }
 
             // add number to vector
             if has_symbol_adjacent {
-                vector_of_numbers.push(
-                    m.as_str().parse::<u64>().expect("No conversion possible")
-                    );
+                vector_of_numbers.push(m.as_str().parse::<u64>().expect("No conversion possible"));
             }
 
             // println!(
@@ -100,9 +82,7 @@ fn do_the_thing(file_path: &str) {
         }
     }
 
-    println!("Total number is {}",
-             vector_of_numbers.iter().sum::<u64>()
-    );
+    println!("Total number is {}", vector_of_numbers.iter().sum::<u64>());
 }
 
 fn is_special_char(c: char) -> bool {
