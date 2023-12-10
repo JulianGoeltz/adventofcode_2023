0a45bfd76c0da52749c1d2efe1e0ae3f3630dbb6
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-12-10 15:06:32 +0100

    04: 02

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,10 +5,80 @@ fn main() {
     let tmp = collect_points(file_path);
     println!("Sum of points is {tmp}");
     assert_eq!(tmp, 13);
+
     let file_path = "inputs/04_input.txt";
     let tmp = collect_points(file_path);
     println!("Sum of points is {tmp}");
     assert_eq!(tmp, 21959);
+
+    println!("============== Counting cards");
+    let file_path = "inputs/04_dummy.txt";
+    let tmp = count_cards(file_path);
+    println!("total number of cards is {tmp}");
+    assert_eq!(tmp, 30);
+
+    let file_path = "inputs/04_input.txt";
+    let tmp = count_cards(file_path);
+    println!("total number of cards is {tmp}");
+    assert_eq!(tmp, 5132675);
+}
+
+fn count_cards(file_path: &str) -> u64 {
+    let contents = fs::read_to_string(file_path).expect("Can't read file");
+    let mut number_of_cards: u64 = 0;
+    let mut multiplier: Vec<u64> = Vec::new();
+    for (_i_card, line) in contents.lines().enumerate() {
+        let mut counting_wins: Vec<bool> = Vec::new();
+        let game_stats = line.split(':').collect::<Vec<&str>>()[1];
+        let tmp = game_stats.split('|').collect::<Vec<&str>>();
+        let string_winning = tmp[0];
+        let string_having = tmp[1];
+        let numbers_winning = string_winning
+            .trim()
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| {
+                s.to_string()
+                    .parse::<u64>()
+                    .expect("no conversion possible")
+            });
+        let numbers_having: Vec<u64> = string_having
+            .trim()
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| {
+                s.to_string()
+                    .parse::<u64>()
+                    .expect("no conversion possible")
+            })
+            .collect();
+        for nw in numbers_winning {
+            if numbers_having.contains(&nw) {
+                counting_wins.push(true);
+            }
+        }
+        let wins: u64 = counting_wins.len().try_into().unwrap();
+        let copies: u64 = (1 + multiplier.len()).try_into().unwrap();
+        number_of_cards += copies;
+        // println!("before cleaning");
+        // println!("{}", multiplier.iter().map(|x| {x.to_string()}).collect::<Vec<String>>().join(" "));
+        multiplier = multiplier
+            .iter()
+            .filter(|x| x >= &&2)
+            .map(|x| x - 1)
+            .collect();
+        // println!("after cleaning");
+        // println!("{}", multiplier.iter().map(|x| {x.to_string()}).collect::<Vec<String>>().join(" "));
+        if wins > 0 {
+            for _ in 0..copies {
+                multiplier.push(wins)
+            }
+        }
+
+        // println!("card {} has {copies} copies and {wins} wins; current number_of_cards {number_of_cards}", _i_card + 1);
+    }
+
+    number_of_cards
 }
 
 fn collect_points(file_path: &str) -> u64 {
